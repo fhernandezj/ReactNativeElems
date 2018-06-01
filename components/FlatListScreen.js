a79c43d8c7cbc0e9997171319a907d7d745bb92c
Francisco Hernandez <fhernandezj@uninorte.edu.co>
2018-05-31 22:12:02 -0500

    Flat List swipe to delete

--- components/FlatListScreen.js
+++ components/FlatListScreen.js
@@ -5,32 +5,69 @@ import { Button,
     YellowBox,
     StyleSheet,
     FlatList,
-    Image
+    Image,
+    Alert
 } from 'react-native';
 import { createStackNavigator } from 'react-navigation'; // Version can be specified in package.json
+import Swipeout from 'react-native-swipeout'
 YellowBox.ignoreWarnings(['Warning: isMounted(...) is deprecated', 'Module RCTImageLoader']);
 
 import flatListData from '../data/flatListData'
 
 class FlatListItem extends React.Component {
     render(){
+        const swipeSettings = {
+            autoClose: true,
+            onClose: (secId, rowId, direction) => {
+                if(this.state.activeRowKey != null){
+                    this.setState({activeRowKey: null});
+                }
+            },
+            onOpen: (secId, rowId, direction) => {
+                this.setState({activeRowKey: this.props.item.key});
+            },
+            right: [
+                {
+                    onPress: () => {
+                        const deletingRow = this.state.activeRowKey
+                        Alert.alert(
+                            'Alert',
+                            'Are you sure you want to delete?',
+                            [
+                                {text: 'No', onPress: () => console.log('Cancel pressed'), style: 'cancel'},
+                                {text: 'Yes', onPress: () => {
+                                    flatListData.splice(this.props.index, 1);
+                                    this.props.parentFlatList.refreshFlatList(deletingRow);
+                                }}
+                            ],
+                            {cancelable: true}
+                        )
+                    },
+                    text: 'Delete', type: 'delete'
+                }
+            ],
+            rowId: this.props.index,
+            sectionId: 1
+        };
         return(
-            <View style={{
-                flex: 1,
-                flexDirection: 'row',
-                backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen' : 'tomato'
-            }}>
-                <Image
-                    source={{uri: this.props.item.url}}
-                    style={{width: 100, height: 100, margin: 5}}/>
+            <Swipeout {...swipeSettings}>
                 <View style={{
                     flex: 1,
-                    flexDirection: 'column'
+                    flexDirection: 'row',
+                    backgroundColor: this.props.index % 2 == 0 ? 'mediumseagreen' : 'tomato'
                 }}>
-                    <Text style={styles.flatListItem}>{this.props.item.name}</Text>
-                    <Text style={styles.flatListItem}>{this.props.item.foodDescription}</Text>
+                    <Image
+                        source={{uri: this.props.item.url}}
+                        style={{width: 100, height: 100, margin: 5}}/>
+                    <View style={{
+                        flex: 1,
+                        flexDirection: 'column'
+                    }}>
+                        <Text style={styles.flatListItem}>{this.props.item.name}</Text>
+                        <Text style={styles.flatListItem}>{this.props.item.foodDescription}</Text>
+                    </View>
                 </View>
-            </View>
+            </Swipeout>
         );
     }
 }
@@ -44,8 +81,18 @@ const styles = StyleSheet.create({
 });
 
 export default class BasicFlatList extends React.Component {
-    componentWillMount(){
-        console.log(flatListData)
+    constructor(props){
+        super(props);
+        this.state = ({
+            deletedRowKey: null
+        });
+    }
+    refreshFlatList = (deletedKey) => {
+        this.setState((prevState) => {
+            return {
+                deletedRowKey: deletedKey
+            };
+        })
     }
     static navigationOptions = {
         title: 'Flat List',
@@ -57,7 +104,7 @@ export default class BasicFlatList extends React.Component {
                     data={flatListData}
                     renderItem={({item, index}) => {
                         return (
-                            <FlatListItem item={item} index={index}>
+                            <FlatListItem item={item} index={index} parentFlatList={this}>
                             </FlatListItem>
                         );
                     }}>
